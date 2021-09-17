5bf72774fc19ba4b125815b7ed0b715421d19fde
wulichao <13248250503@163.com>
2021-09-17 22:55:34 +0800

    Java 学习9.17

--- src/MyDuoTai/AnimalDemo.java
+++ src/MyDuoTai/AnimalDemo.java
@@ -2,6 +2,10 @@ package MyDuoTai;
 
 public class AnimalDemo {
     public static void main(String[] args) {
-        Animal a=new Cat();
+        AnimalOperator ao=new AnimalOperator();
+        Cat c=new Cat();
+        Dog d=new Dog();
+        ao.useAnimal(c);
+        ao.useAnimal(d);
     }
 }
---
+++ src/MyDuoTai/AnimalOperator.java
@@ -0,0 +1,15 @@
+package MyDuoTai;
+
+public class AnimalOperator {
+    /*
+    public void useAnimal(Cat c){
+        c.eat();
+    }
+    public void useAnimal(Dog d){
+        d.eat();
+    }
+     */
+    public void useAnimal(Animal a){
+        a.eat();
+    }
+}
