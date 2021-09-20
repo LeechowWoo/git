8e7a048c3ed8c6b2fb599c9520943afb0bce4c2b
wulichao <13248250503@163.com>
2021-09-20 20:11:37 +0800

    Java 学习9.20

---
+++ src/InnerClass/Outer.java
@@ -0,0 +1,15 @@
+package InnerClass;
+
+public class Outer {
+    private int num=10;
+    public class Inner{
+        public void show(){
+            System.out.println(num);
+        }
+    }
+    public void method(){
+        //show();
+        Inner i=new Inner();
+        i.show();
+    }
+}
