d75b3a67c13ab9a43867ff6639136bcbd17cbd9f
wulichao <13248250503@163.com>
2021-09-21 22:32:58 +0800

    Java 学习9.21

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,11 @@
 public class Demo {
     public static void main(String[] args) {
+        long start=System.currentTimeMillis();
+        for(int i=0;i<10000;i++){
+            System.out.println(i);
+        }
+        long end=System.currentTimeMillis();
+        System.out.println("共耗时："+(end-start)+"毫秒");
     }
 }
 
---
+++ src/TestProgramm.java
@@ -0,0 +1,8 @@
+public class TestProgramm {
+    public static void main(String[] args) {
+        Integer i1=Integer.valueOf(100);
+        Integer i2=100;
+        System.out.println(i1);
+        System.out.println(i2);
+    }
+}
