490cfcb204ec6e3fb80999b59f1abb5fe8716ac6
wulichao <13248250503@163.com>
2021-10-10 11:01:22 +0800

    Java 学习10.10y

--- src/Demo.java
+++ src/Demo.java
@@ -1,11 +1,14 @@
 public class Demo {
     public static void main(String[] args) {
-        int[][] dp = new int[11][11];
-        for(int i=0;i<11;i++){
-            for(int j=0;j<11;j++){
-                System.out.println(dp[i][j]);
+        int n=25;
+        int ans=0;
+        String s = Integer.toBinaryString(n);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==1){
+                ans++;
             }
         }
+        System.out.println(ans);
     }
 }
 
